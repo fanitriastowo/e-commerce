09947af5592b81baf38bd72ed7daa6835abd82de
fanitriastowo <fani.triastowo@gmail.com>
2015-09-22 21:29:00 +0700

    initial commit

---
+++ application/logs/log-2015-09-22.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2015-09-22 10:26:58 --> Config Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Hooks Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Utf8 Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> UTF-8 Support Enabled
+DEBUG - 2015-09-22 10:26:58 --> URI Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Router Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> No URI present. Default controller set.
+DEBUG - 2015-09-22 10:26:58 --> Output Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Security Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Input Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-09-22 10:26:58 --> Language Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Loader Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> Controller Class Initialized
+DEBUG - 2015-09-22 10:26:58 --> File loaded: application/views/home.php
+DEBUG - 2015-09-22 10:26:58 --> Final output sent to browser
+DEBUG - 2015-09-22 10:26:58 --> Total execution time: 0.0305
---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<?php include 'template/css.php'; ?>
+</head>
+<body>
+
+<div class="container">
+	<h1>Welcome to CodeIgniter!</h1>
+</div>
+
+<?php include 'template/js.php'; ?>
+</body>
+</html>

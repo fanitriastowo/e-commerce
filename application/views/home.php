9598e1cd46261c1181495059a9cb71df77529f3c
fanitriastowo <fani.triastowo@gmail.com>
2015-09-24 13:21:59 +0700

    add navbar using bootstrap

--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
 	<?php include 'template/css.php'; ?>
+	<title>Welcome to CodeIgniter</title>
 </head>
 <body>
-
+<?php include 'template/navbar.php'; ?>
 <div class="container">
 	<h1>Welcome to CodeIgniter!</h1>
 </div>
--- application/views/template/css.php
+++ application/views/template/css.php
@@ -1,12 +1,16 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="assets/images/favicon.ico">
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 <!--[if lt IE 9]>
-	  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->

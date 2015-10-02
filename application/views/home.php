d66001eb9692ad2ada8322d3805c9bf919ad5832
fanitriastowo <fani.triastowo@gmail.com>
2015-10-02 21:35:05 +0700

    add basic template in home

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!-- CSS -->
 	<?php $this->load->view('template/css.php'); ?>
 	<title>Selamat datang pengujung</title>
 </head>
 <body>
+	<!-- Navbar -->
 	<?php $this->load->view('template/navbar.php'); ?>
 	<div class="container">
-		<h1>Welcome to CodeIgniter!</h1>
+		<div class="row">
+			<h1 class="text-center">Header</h1>
+		</div>
+		<div class="row">
+			<div class="col-sm-2">
+				<h3 class="text-center">Sidebar</h3>
+			</div>
+			<div class="col-sm-8">
+				<h3 class="text-center">Welcome to CodeIgniter!</h3>
+			</div>
+			<div class="col-sm-2">
+				<h3 class="text-center">Latest Product</h3>
+			</div>
+		</div>
 	</div>
 
+	<!-- Footer -->
 	<?php $this->load->view('template/footer.php'); ?>
+
+	<!-- Javascript -->
 	<?php $this->load->view('template/js.php'); ?>
 </body>
 </html>
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,18 +1,12 @@
  <div class="e-masthead">
  	<div class="container">
  		<div class="navbar-header">
- 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
- 				<span class="sr-only">Toggle navigation</span>
- 				<span class="icon-bar"></span>
- 				<span class="icon-bar"></span>
- 				<span class="icon-bar"></span>
- 			</button>
  			<a class="e-navbar-brand" href="#">E - Randu Jati</a>
  		</div>
  		<form class="navbar-form navbar-left">
-			<div class="form-group">
- 				<input type="text" placeholder="Pencarian : Produk . . ." class="form-control">
-			</div>
+ 			<div class="form-group">
+ 				<input type="text" placeholder="Pencarian : Produk . . ." class="form-control" name="search">
+ 			</div>
  		</form>
  		<div class="navbar-right">
  			<nav class="e-nav">

d6fe702c8458735064d052d2a85183a8233d25ed
fanitriastowo <fani.triastowo@gmail.com>
2015-10-05 09:49:36 +0700

    add logo

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,7 +1,7 @@
  <div class="e-masthead">
  	<div class="container">
  		<div class="navbar-header">
- 			<a class="e-navbar-brand" href="#">E - Randu Jati</a>
+ 			<a class="e-navbar-brand" href='<?php echo site_url(); ?>'>e - Randu Jati</a>
  		</div>
  		<div class="navbar-left">
  			<nav class="e-nav">
@@ -12,7 +12,7 @@
  		<div class="navbar-right">
  			<nav class="e-nav">
  				<a class="btn btn-default" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 0</a>
- 				<a class="e-nav-item active" href="#">Your name</a>
+ 				<a class="e-nav-item" href="#">Your name</a>
  				<a class="e-nav-item" href="#">Registration</a>
  				<a class="e-nav-item" href="#">Logout</a>
  			</nav>

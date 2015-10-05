9f8c2da41bbc1f10a4a78888b64f97f9d3f46d62
fanitriastowo <fani.triastowo@gmail.com>
2015-10-05 22:04:33 +0700

    finish login view

---
+++ application/views/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- CSS -->
+	<?php $this->load->view('template/css'); ?>
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/login.css'); ?>">
+	<title>Please Login</title>
+</head>
+<body>
+	<!-- Navbar -->
+	<?php $this->load->view('template/navbar'); ?>
+	<div class="container">
+		<div class="row">
+			<!-- Main Content (Login Form) -->
+			<div class="col-sm-offset-2 col-sm-8">
+				<?php echo form_open('', array('class' => 'form-signin')); ?>
+					<h2 class="form-signin-heading">Please sign in</h2>
+
+					<?php echo form_label('Username', 'username', array('class' => 'sr-only')); ?>
+					<?php echo form_input(array('name' => 'username', 'id' => 'username', 'value' => '', 'class' => 'form-control', 'placeholder' => 'Username', 'required' => '', 'autofocus' => '')); ?>
+
+					<?php echo form_label('Password', 'password', array('class' => 'sr-only')); ?>
+					<?php echo form_password(array('name' => 'password', 'id' => 'password', 'value' => '', 'class' => 'form-control', 'placeholder' => 'Password', 'required' => '')); ?>
+
+					<?php echo form_submit('submit', 'Login', 'class = "btn btn-lg btn-primary btn-block"'); ?>
+				<?php echo form_close(); ?>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<?php $this->load->view('template/footer'); ?>
+
+	<!-- Javascript -->
+	<?php $this->load->view('template/js'); ?>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#login').addClass('active');
+		});
+	</script>
+</body>
+</html>
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -8,6 +8,7 @@
  				<a class="btn btn-default" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 0</a>
  				<a class="e-nav-item" href="#">Your name</a>
  				<a class="e-nav-item" href="#">Registration</a>
+ 				<a id="login" class="e-nav-item" href="<?php echo site_url('login'); ?>">Login</a>
  				<a class="e-nav-item" href="#">Logout</a>
  			</nav>
  		</div>

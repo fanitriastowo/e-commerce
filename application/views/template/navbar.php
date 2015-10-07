2201b520597a98ccc8d93ac3f24d3e8fa86e66f7
fanitriastowo <fani.triastowo@gmail.com>
2015-10-07 17:17:11 +0700

    change semantic html to php template tag

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,19 +1,19 @@
  <div class="e-masthead">
  	<div class="container">
  		<div class="navbar-header">
- 			<a class="e-navbar-brand" href='<?php echo site_url(); ?>'><span class="fa fa-lg fa-internet-explorer"></span> - Randu Jati</a>
+ 			<?php echo anchor('/', '<span class="fa fa-lg fa-internet-explorer"></span> - Randu Jati', 'class="e-navbar-brand"'); ?>
  		</div>
  		<div class="navbar-right">
  			<nav class="e-nav">
  				<?php if (!$this->ion_auth->logged_in()): ?>
- 					<a id="registration" class="e-nav-item" href="#" data-toggle="modal" data-target="#registration_modal">Registration</a>
- 					<a id="login" class="e-nav-item" href="<?php echo site_url('user/login'); ?>">Login</a>
+ 					<?php echo anchor('#', 'Registration', 'id="registration" class="e-nav-item" title="Input your registration identity" data-toggle="modal" data-target="#registration_modal"'); ?>
+ 					<?php echo anchor('user/login', 'Login', 'id="login" class="e-nav-item"'); ?>
  				<?php endif ?>
  				
  				<?php if ($this->ion_auth->logged_in()): ?>
- 					<a class="btn btn-default" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 0</a>
- 					<a id="profile" class="e-nav-item" href="#">Your Profile</a>
- 					<a class="e-nav-item" href="<?php echo site_url('user/logout'); ?>">Logout</a>
+ 					<?php echo anchor('#', '<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 0', 'class="btn btn-default"'); ?>
+ 					<?php echo anchor('#', 'Your Profile', 'id="profile" class="e-nav-item"'); ?>
+ 					<?php echo anchor('user/logout', 'Logout', 'class="e-nav-item"'); ?>
  				<?php endif ?>
  			</nav>
  		</div>
@@ -21,6 +21,7 @@
  </div>
 
  <!-- Modal -->
+ <?php echo form_open('', 'class = form-horizontal'); ?>
  <div class="modal fade" id="registration_modal" tabindex="-1" role="dialog" aria-labelledby="registration_modal_label">
  	<div class="modal-dialog" role="document">
  		<div class="modal-content">
@@ -29,35 +30,34 @@
  				<h4 class="modal-title" id="registration_modal_label">Input your identity</h4>
  			</div>
  			<div class="modal-body">
- 				<?php echo form_open('', 'class = form-horizontal'); ?>
 
  				<div class="form-group">
- 					<label for="register_username" class="col-sm-2 control-label">Username</label>
+	 				<?php echo form_label('Username', 'register_username', array('class' => 'col-sm-2 control-label')); ?>
  					<div class="col-sm-10">
- 						<input type="input" class="form-control" id="register_username" placeholder="Username">
+ 						<?php echo form_input('username','', 'class="form-control" id="register_username" placeholder="Username"'); ?>
  					</div>
  				</div>
 
  				<div class="form-group">
- 					<label for="register_email" class="col-sm-2 control-label">Email</label>
+	 				<?php echo form_label('Email', 'register_email', array('class' => 'col-sm-2 control-label')); ?>
  					<div class="col-sm-10">
- 						<input type="email" class="form-control" id="register_email" placeholder="Email">
+ 						<?php echo form_input('email','', 'class="form-control" id="register_email" placeholder="Email"'); ?>
  					</div>
  				</div>
 
  				<div class="form-group">
- 					<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+ 					<?php echo form_label('Password', 'register_password', array('class' => 'col-sm-2 control-label')); ?>
  					<div class="col-sm-10">
- 						<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+ 						<?php echo form_password('password','', 'class="form-control" id="register_password" placeholder="Password"'); ?>
  					</div>
  				</div>
 
- 				<?php echo form_close(); ?>
  			</div>
  			<div class="modal-footer">
  				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
- 				<button type="button" class="btn btn-primary">Register</button>
+ 				<?php echo form_submit('submit', 'Register', 'class = "btn btn-primary"'); ?>
  			</div>
  		</div>
  	</div>
  </div>
+ <?php echo form_close(); ?>

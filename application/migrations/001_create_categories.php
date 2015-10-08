7009fb79814f96eaa4ae1eb0658baff0d8f1dd9e
fanitriastowo <fani.triastowo@gmail.com>
2015-10-08 18:20:48 +0700

    enhanced migration for categories and products

--- application/migrations/001_create_categories.php
+++ application/migrations/001_create_categories.php
@@ -18,18 +18,29 @@ class Migration_Create_categories extends CI_Migration {
 			)
 		));
 		$this->dbforge->add_key('id', TRUE);
-		$this->dbforge->create_table('CATEGORIES');*/
+		$this->dbforge->create_table('categories');*/
 		
 		// code dibawah 100% sama dengan code diatas
-		$query = "CREATE TABLE CATEGORIES (
-				id INT(11) NOT NULL AUTO_INCREMENT,
-				name VARCHAR(100) NOT NULL,
+		$query = "CREATE TABLE `categories` (
+				`id` INT(11) NOT NULL AUTO_INCREMENT,
+				`name` VARCHAR(100) NOT NULL,
 				PRIMARY KEY (id))";
+
+		$insert_meja = "INSERT INTO `categories` SET name='Meja'";
+		$insert_kursi = "INSERT INTO `categories` SET name='Kursi'";
+		$insert_lemari = "INSERT INTO `categories` SET name='Lemari'";
+		$insert_kasur = "INSERT INTO `categories` SET name='Kasur'";
+		$insert_set = "INSERT INTO `categories` SET name='Special Set'";
 		
 		$this->db->query($query);
+		$this->db->query($insert_meja);
+		$this->db->query($insert_kursi);
+		$this->db->query($insert_lemari);
+		$this->db->query($insert_kasur);
+		$this->db->query($insert_set);
 	}
 
 	public function down() {
-		$this->dbforge->drop_table('CATEGORIES');
+		$this->dbforge->drop_table('categories');
 	}
 }

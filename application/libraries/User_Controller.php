3e8ea861dc0c256011d34e45984e6bca1e8db3f6
fanitriastowo <fani.triastowo@gmail.com>
2015-10-12 22:55:23 +0700

    fixing admin and user security bug

--- application/libraries/User_Controller.php
+++ application/libraries/User_Controller.php
@@ -8,7 +8,7 @@ class User_Controller extends MY_Controller {
 		$exception = array('user/user/login', 'user/user/post_login', 'user/user/logout', 'user/user/registration');
 		if ($this->ion_auth->logged_in() == FALSE && $this->ion_auth->in_group('members') == FALSE && !in_array(uri_string(), $exception)) {
             redirect('user/user/login');
-        } else if ($this->ion_auth->is_admin() == TRUE && !in_array(uri_string(), $exception)) {
+        } else if ($this->ion_auth->logged_in() == TRUE && $this->ion_auth->is_admin() == TRUE && !in_array(uri_string(), $exception)) {
         	show_404();
         }
 	}

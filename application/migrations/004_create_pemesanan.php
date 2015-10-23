2455ce1e53dae7db2a5eb014819663ed723d1cfe
fanitriastowo <fani.triastowo@gmail.com>
2015-10-23 20:39:17 +0700

    one-to-many from pemesanan to product

--- application/migrations/004_create_pemesanan.php
+++ application/migrations/004_create_pemesanan.php
@@ -6,15 +6,19 @@ class Migration_Create_Pemesanan extends CI_Migration {
 		$query = "CREATE TABLE `pemesanan` (
 				  `id` INT(11) NOT NULL,
 				  `pemesanan_id` VARCHAR(45) NOT NULL,
+				  `user_id` INT(11) UNSIGNED NOT NULL,
+				  `product_id` INT(11) NOT NULL,
 				  `name` VARCHAR(100) NOT NULL,
 				  `qty` INT(11) NOT NULL,
 				  `price` BIGINT(15) NOT NULL,
 				  `subtotal` BIGINT(15) NOT NULL,
-				  `user_id` INT(11) UNSIGNED NOT NULL,
 				  PRIMARY KEY (`id`),
-				  INDEX `fk_user_id_idx` (`user_id` ASC),
+				  KEY `fk_user_id_idx` (`user_id`),
+				  KEY `fk_product_id_idx` (`product_id`),
 				  	CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`)
-				    REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION)";
+				    REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+				    CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`)
+				    REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION)";
 		$this->db->query($query);
 	}
 	public function down() {
